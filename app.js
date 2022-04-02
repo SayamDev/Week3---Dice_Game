fc94daace943228e2aa19155d3a11c3854a768d2
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-02 16:54:02 +0100

    produced imgs randomly

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 
-let randomDiceNumber = Math.floor((Math.random() *6) + 1);
 
 
-console.log(randomDiceNumber)
+
+
+
+
+dice_btn.addEventListener("click", () => {
+    let randomDiceNumber = Math.floor((Math.random() *6) + 1);
+    
+    let DiceImages = "images/dice" + randomDiceNumber + ".png";
+
+    document.getElementById("roll_1").setAttribute('src', DiceImages)
+
+})
+

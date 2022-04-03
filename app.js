0c1af287c10eea67bbfe42ced5f82220469a1cca
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-03 17:10:34 +0100

    random number + added score

--- app.js
+++ app.js
@@ -5,31 +5,40 @@
 //declaring elements
 let diceButton = document.getElementById("dice_btn");
 let diceImage = document.getElementById("diceImage");
+let urScore = document.getElementById("your_score");
 
-//  6 images stored inside the imgs inside the object
-const imgs = {
+const diceSound = document.getElementById("diceSound")
+
+
+
+//  6 images stored inside the sixImgs object with the assgined number properties (object initializer)
+const sixImgs = {
     1:"images/dice1.png",
     2:"images/dice2.png",
     3:"images/dice3.png",
     4:"images/dice4.png",
     5:"images/dice5.png",
     6:"images/dice6.png",
-}
+};
 
 
 
+//assigning variables
+let diceRoll = 0;
+let score = 0;
 
-
-/*
-dice_btn.addEventListener("click", () => {
-    let randomDiceNumber = Math.floor((Math.random() *6) + 1);
-
-    let DiceImages = "images/dice" + randomDiceNumber + ".png";
-
-    document.getElementById("roll_1").setAttribute('src', DiceImages);
-
-    DiceImages += roll;
-    h2.texContent = score;
-
+diceButton.addEventListener("click", () => { //added click event to button
+    let diceRoll = Math.floor(Math.random() *6 + 1); //will generate random numbers between 1-6
+    diceImage.src = sixImgs[diceRoll]; //targeted "diceImage" src attribute , allowing me to return the value src attributes of the images inside the id = diceImage & then assigning diceRoll to the object sixImgs
+    score += diceRoll; // adding the value of diceRoll to score variable
+    urScore.textContent = score; // will show the result of score in the "your_score element int the HTML"
+    urScore.style.marginLeft = "20px";//style element
+    urScore.style.paddingLeft = "100px";//style element
+    urScore.style.fontSize = "3rem";//style element
+    diceSound.src = "sound/roll.wav";//dice sound
 })
-*/
+
+
+
+
+

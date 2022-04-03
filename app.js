fa0e25bdfb24b8f0f97c16537f3cfdc357b5faff
Sayam Ajmal <itssanesayam@gmail.com>
2022-04-03 15:47:18 +0100

    restart

--- app.js
+++ app.js
@@ -2,15 +2,34 @@
 
 
 
+//declaring elements
+let diceButton = document.getElementById("dice_btn");
+let diceImage = document.getElementById("diceImage");
+
+//  6 images stored inside the imgs inside the object
+const imgs = {
+    1:"images/dice1.png",
+    2:"images/dice2.png",
+    3:"images/dice3.png",
+    4:"images/dice4.png",
+    5:"images/dice5.png",
+    6:"images/dice6.png",
+}
 
 
 
+
+
+/*
 dice_btn.addEventListener("click", () => {
     let randomDiceNumber = Math.floor((Math.random() *6) + 1);
-    
+
     let DiceImages = "images/dice" + randomDiceNumber + ".png";
 
-    document.getElementById("roll_1").setAttribute('src', DiceImages)
+    document.getElementById("roll_1").setAttribute('src', DiceImages);
+
+    DiceImages += roll;
+    h2.texContent = score;
 
 })
-
+*/
